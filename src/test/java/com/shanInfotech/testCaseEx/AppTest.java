0dbc6018c868a1e01cdfb0711415bf56bbe75f86
Yogeshhampa <hcyogesh753@gmail.com>
2025-07-08 15:20:14 +0530

    test case

--- src/test/java/com/shanInfotech/testCaseEx/AppTest.java
+++ src/test/java/com/shanInfotech/testCaseEx/AppTest.java
@@ -22,7 +22,7 @@ public class AppTest {
     public void testArea() {
     	Calculate c=new Calculate();
     	assertEquals(12, c.areaOfCircle(2));
-    	assertEquals(9, c.areaOfCircle(3));
+    	assertEquals(28, c.areaOfCircle(3));
     }
     
 }
